3ff059a60befdc4bb5346fbb5819d300ad838824
Yujian <381519746@qq.com>
2017-08-10 16:42:18 +0800

    plus

---
+++ src/Cal.java
@@ -0,0 +1,29 @@
+
+public class Cal {
+
+	public int num1;
+	public int num2;
+	public int sum;
+	
+	public Cal(int number1,int number2) {
+		// TODO Auto-generated constructor stub
+		this.num1=number1;
+		this.num2=number2;
+	}
+
+	public int getNum1() {
+		return num1;
+	}
+
+	public int getNum2() {
+		return num2;
+	}
+
+	public int additive(){
+		sum = num1+num2;
+		return sum;
+	}
+	
+	
+	
+}
