46b86f49a326691cbfa801c2b23e33023bef08f6
Yujian <381519746@qq.com>
2017-08-11 10:50:59 +0800

    231

--- src/main/java/wps/Test.java
+++ src/main/java/wps/Test.java
@@ -6,9 +6,8 @@ public class Test {
 
 	@org.junit.Test
 	public void test() {
-//        Cal cal = new Cal(1, 3);
-//        System.out.println(""+cal.additive());
-        assertTrue(true);
+        Cal cal = new Cal(1, 3);
+        System.out.println(""+cal.additive());
 	}
 
 }
